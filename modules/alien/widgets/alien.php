ce3a47d68f672220169b2fb4959c5322653eeacf
usuf-bdt <usufbdt@gmail.com>
2025-12-23 17:30:04 +0600

    Button alignment control added in the alien widget

--- modules/alien/widgets/alien.php
+++ modules/alien/widgets/alien.php
@@ -80,6 +80,34 @@ class Alien extends Module_Base {
 		$this->register_lightbox_controls();
 		$this->register_link_target_controls();
 
+		$this->add_responsive_control(
+			'button_alignment',
+			[
+				'label'   => esc_html__('Button Alignment', 'pixel-gallery') . BDTPG_NC,
+				'type'    => Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => esc_html__('Left', 'pixel-gallery'),
+						'icon'  => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => esc_html__('Center', 'pixel-gallery'),
+						'icon'  => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => esc_html__('Right', 'pixel-gallery'),
+						'icon'  => 'eicon-text-align-right',
+					],
+				],
+				'devices'      => ['desktop', 'tablet'],
+				'prefix_class' => 'button-align-%s',
+				'condition'    => [
+					'link_to!' 	  => 'none',
+					'link_target' => 'only_button',
+				],
+			]
+		);
+
 		$this->end_controls_section();
 
 		$this->start_controls_section(
