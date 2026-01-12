fea196ca969dc6df3294b8c2963e070a0826a93a
usuf-bdt <usufbdt@gmail.com>
2026-01-12 11:00:31 +0600

    Issue fixed

--- src/admin/js/pg-biggopti.js
+++ src/admin/js/pg-biggopti.js
@@ -14,7 +14,6 @@ jQuery(document).ready(function ($) {
                 meta: $meta,
                 time: $time,
                 _wpnonce: PixelGalleryBiggoptiConfig.nonce,
-                current_url: window.location.href
             }
         });
     });
@@ -111,7 +110,8 @@ jQuery(document).ready(function ($) {
                 dataType: 'json',
                 data: {
                     action: 'pg_fetch_api_biggopties',
-                    _wpnonce: PixelGalleryBiggoptiConfig.nonce
+                    _wpnonce: PixelGalleryBiggoptiConfig.nonce,
+                    current_url: window.location.href
                 }
             })
             .done(function (res) {
