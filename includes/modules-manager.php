33248cab1e55996ffc9d6562018b69ae725a6dd8
Faridul Islam <faridbpi10@gmail.com>
2026-01-14 17:40:55 +0600

    removed rtl css files

--- includes/modules-manager.php
+++ includes/modules-manager.php
@@ -69,9 +69,7 @@ final class Manager
                 if (ModuleService::has_module_style($module_id)) {
                     wp_register_style('pg-' . $module_id, BDTPG_URL . 'assets/css/pg-' . $module_id . '.css', [], BDTPG_VER);
                 }
-                // if (ModuleService::has_module_style($module_id)) {
-                //     wp_register_style('pg-' . $module_id, BDTPG_URL . 'assets/css/pg-' . $module_id . $direction . '.css', [], BDTPG_VER);
-                // }
+
                 // register widget JS
                 if (ModuleService::has_module_script($module_id)) {
                     wp_register_script('pg-' . $module_id, BDTPG_URL . 'assets/js/modules/pg-' . $module_id . '.min.js', ['jquery', 'elementor-frontend'], BDTPG_VER, true);
